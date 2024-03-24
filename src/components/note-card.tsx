53d4660b1aa90c2c8ec7d54cacc501da6cd8c71d
gabriel matias <gmsilva1@uniara.edu.br>
2024-03-24 15:02:06 -0300

    card inicial criado

---
+++ src/components/note-card.tsx
@@ -0,0 +1,9 @@
+export function NoteCard() {
+    return(
+        <div className='rouded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative '> 
+        <span className='text-sm font-medium text-slate-200'>há 2 dias</span> 
+        <p className='text-sm leading-6 text-slate-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta deserunt atque illum reprehenderit cumque, quam nobis sit neque corporis hic tempora perferendis amet, tenetur ratione ex, veniam animi repellendus. Ducimus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta deserunt atque illum reprehenderit cumque, quam nobis sit neque corporis hic tempora perferendis amet, tenetur ratione ex, veniam animi repellendus. Ducimus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta deserunt atque illum reprehenderit cumque, quam nobis sit neque corporis hic tempora perferendis amet, tenetur ratione ex, veniam animi repellendus. Ducimus? </p>
+        <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none'/>
+        </div>
+    )
+}
